6c06e88e0377c7d1ace0e2b7d4adf30e71f91580
kangdaehoon <eogns47@konkuk.ac.kr>
2024-01-31 17:48:36 +0900

    🤔test: jenkins CI test

--- src/test/java/Potatonet/ScanServer/ScanServerApplicationTests.java
+++ src/test/java/Potatonet/ScanServer/ScanServerApplicationTests.java
@@ -3,7 +3,7 @@ package Potatonet.ScanServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ScanServerApplicationTests {
 
 	@Test
